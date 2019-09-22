bda80378d54083629c011889303f3082ddbe60d8
DunkRing <Chris31@live.dk>
2019-09-22 16:38:27 +0200

    params of create booking

--- Classes/Bookings.cs
+++ Classes/Bookings.cs
@@ -1,5 +1,18 @@
-class Bookings 
+class Booking 
 {
+    /// <summary>
+    /// Creates a Booking, also the constructor of Booking
+    /// </summary>
+    /// <param name="bookingId">The Id of the booking, makes the booking unique.</param>
+    /// <param name="carId">The id of the car to associate a booking with a car.</param>
+    /// <param name="customerId">The id of the custumer to associate a booking with a customer.</param>
+    /// <param name="pickUpLocation">The pickup location.</param>
+    /// <param name="pickupTime">The pickup location.</param>
+    public Booking(int bookingId, int carId, int customerId, string pickUpLocation, DateTime pickupTime)
+    {
+          
+    }
+    
     /// <summary>
     /// Returns a list of bookings owned by the driver with the given driverlicense.
     /// </summary>
