d15bbc6441385a1435110cfb8fa8d5e3c89f4964
Mikkel Wexøe Ertbjerg <44799207+mikkelertbjerg@users.noreply.github.com>
2019-09-23 09:58:36 +0200

    Update ICar.cs

--- Classes/ICar.cs
+++ Classes/ICar.cs
@@ -1,4 +1,9 @@
-public interface ICar{
+public interface ICar {
   
+  /// <summary>
+  /// Returns a list of available cars.
+  /// </summary>
+  /// <returns>Returns a list of available cars.</returns>
+  public List<Car> ShowAvailableCars() {}
 
 }
