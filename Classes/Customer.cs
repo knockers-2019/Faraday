6ba2c08898ecafc71ac2e87d4a4f8ab69b46e83d
Mikkel Wexøe Ertbjerg <44799207+mikkelertbjerg@users.noreply.github.com>
2019-09-23 10:39:31 +0200

    Update Customer.cs

--- Classes/Customer.cs
+++ Classes/Customer.cs
@@ -1,9 +1,11 @@
 class Customer {
 
   private string driversLicense;
+  private string name;
 
-  public Customer(string diversLicense){
+  public Customer(string diversLicense, string name){
     this.driversLicense = driversLicense;
+    this.name = name;
   }
 
   public Customer(){
@@ -11,5 +13,6 @@ class Customer {
   }
 
   public string driversLicense {get; set;}
+  public string name {get; set;}
 
 }
