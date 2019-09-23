57fd10ba1b4b657fdbe035e19657bd8594fe507f
Mikkel Wexøe Ertbjerg <44799207+mikkelertbjerg@users.noreply.github.com>
2019-09-23 09:23:24 +0200

    Update Booking.cs
    Added empty constructor.
    Added private fields.
    Added getters and setters for all props.

--- Classes/Booking.cs
+++ Classes/Booking.cs
@@ -8,8 +8,31 @@ class Booking
     /// <param name="customerId">The id of the custumer to associate a booking with a customer.</param>
     /// <param name="pickUpLocation">The pickup location.</param>
     /// <param name="pickupTime">The pickup location.</param>
-    public Booking(int bookingId, int carId, int customerId, string pickUpLocation, DateTime pickupTime)
+    
+    private int bookingId;
+    private int carId;
+    private int customerId;
+    private string pickupLocation;
+    private DateTime pickupTime;
+    
+    public Booking(int bookingId, int carId, int customerId, string pickupLocation, DateTime pickupTime)
     {
-
+        this.bookingId = bookingId;
+        this.carId = carId;
+        this.customerId = customerId;
+        this.pickupLocation = pickupLocation;
+        this.puckupTime = pickupTime;
     }
+    
+    /// Empty constructor
+    public Booking(){
+    
+    }
+    
+    ///Getters and setters
+    public int bookingId {get; set;}
+    public int carId {get; set;}
+    public int customerId {get; set;}
+    public string pickupLocation {get; set;}
+    public DateTime pickupTime {get; set;}
 }
