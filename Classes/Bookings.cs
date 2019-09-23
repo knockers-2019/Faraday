102fe1d8b41a121220584491f37cf2b6b42e99be
Mikkel Wexøe Ertbjerg <44799207+mikkelertbjerg@users.noreply.github.com>
2019-09-23 09:30:44 +0200

    Update Bookings.cs
    changed the create booking header

--- Classes/Bookings.cs
+++ Classes/Bookings.cs
@@ -16,10 +16,6 @@ public interface IBooking
     /// <summary>
     /// Creates a Booking, also the constructor of Booking
     /// </summary>
-    /// <param name="bookingId">The Id of the booking, makes the booking unique.</param>
-    /// <param name="carId">The id of the car to associate a booking with a car.</param>
-    /// <param name="customerId">The id of the custumer to associate a booking with a customer.</param>
-    /// <param name="pickUpLocation">The pickup location.</param>
-    /// <param name="pickupTime">The pickup location.</param>
-    public void Booking CreateBooking(int bookingId, int carId, int customerId, string pickUpLocation, DateTime pickupTime) {}
+    /// <param name="booking">The booking that is to be created</param>
+    public void Booking CreateBooking(Booking booking) {}
 }
