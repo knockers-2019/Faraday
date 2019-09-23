e15673b60b664ce4978796b09580e5cd0a69d4fb
Mikkel Wexøe Ertbjerg <44799207+mikkelertbjerg@users.noreply.github.com>
2019-09-23 09:15:46 +0200

    Update Bookings.cs
    Changed the class to an interface instead.
    Added create booking.

--- Classes/Bookings.cs
+++ Classes/Bookings.cs
@@ -1,21 +1,25 @@
-class Bookings 
+public interface IBooking 
 {
     /// <summary>
     /// Returns a list of bookings owned by the driver with the given driverlicense.
     /// </summary>
     /// <param name="driversLicense">The drivers license number to get a booking.</param>
     /// <returns>Returns a list of bookings.</returns>
-    public List<Booking> ShowBookings(string driversLicense)
-    {   
-
-    }
+    public List<Booking> ShowBookings(string driversLicense) {}
 
     /// <summary>
     /// Cancels the booking who owns the bookingId given.
     /// </summary>
     /// <param name="bookingId">The unique Id for a booking</param>
-    public void CancelBooking(int bookingId)
-    {
-
-    }
+    public void CancelBooking(int bookingId) {}
+    
+    /// <summary>
+    /// Creates a Booking, also the constructor of Booking
+    /// </summary>
+    /// <param name="bookingId">The Id of the booking, makes the booking unique.</param>
+    /// <param name="carId">The id of the car to associate a booking with a car.</param>
+    /// <param name="customerId">The id of the custumer to associate a booking with a customer.</param>
+    /// <param name="pickUpLocation">The pickup location.</param>
+    /// <param name="pickupTime">The pickup location.</param>
+    public void Booking CreateBooking(int bookingId, int carId, int customerId, string pickUpLocation, DateTime pickupTime) {}
 }
