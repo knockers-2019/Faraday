9d96eb78c7472d91a6227f82ee4f9bd923d096ee
Mikkel Wexøe Ertbjerg <44799207+mikkelertbjerg@users.noreply.github.com>
2019-09-23 10:15:23 +0200

    Update Car.cs

--- Classes/Car.cs
+++ Classes/Car.cs
@@ -1,1 +1,29 @@
+class Car
+{
+
+  /// <summary>
+  /// The mode class of a car
+  /// </summary>
+  /// <param name="carId">The Id of the car, makes the car unique, and mainly used for convience.</param>
+  /// <param name="licensePlate">A unique identifier, which is used for references in the system.</param>
+  
+  private string carId;
+  private string licensePlate;
+
+  public Car(string carId, string licensePlate) {
+    this.carId = carId;
+    this.licensePlate = licensePlate;
+  }
+
+  ///Empty constructor
+  public Car(){
+
+  }
+
+  ///Getters and setters
+  public string carId {get; set;}
+  public string licensePlate {get; set;}
+
+}
+
 
