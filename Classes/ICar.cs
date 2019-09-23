ea7bb3490dcfdff95a7eed04b3d653550851f2cf
Mikkel Wexøe Ertbjerg <44799207+mikkelertbjerg@users.noreply.github.com>
2019-09-23 10:10:55 +0200

    Update ICar.cs

--- Classes/ICar.cs
+++ Classes/ICar.cs
@@ -4,6 +4,11 @@ public interface ICar {
   /// Returns a list of available cars.
   /// </summary>
   /// <returns>Returns a list of available cars.</returns>
-  public List<Car> ShowAvailableCars() {}
-
+  public List<Car> ShowAvailableCars(string place, DateTime time) {}
+  
+  /// <summary>
+  /// Returns all the details of a specific car.
+  /// </summary>
+  /// <returns>Returns a car object.</returns>
+  public Car getCarDetails(string carId) {}
 }
