9979d130e549f5b48fade74dadbd293f7f6a0aed
JonasManley <jonasmanley@hotmail.com>
2019-10-21 11:19:45 +0200

    Contract dll project added

---
+++ Faraday Contract/Faraday Contract/Booking.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faraday_Contract
+{
+    public class Booking
+    {
+        /// <summary>
+        /// Creates a Booking, also the constructor of Booking
+        /// </summary>
+        /// <param name="bookingId">The Id of the booking, makes the booking unique.</param>
+        /// <param name="carId">The id of the car to associate a booking with a car.</param>
+        /// <param name="customerId">The id of the custumer to associate a booking with a customer.</param>
+        /// <param name="pickUpLocation">The pickup location.</param>
+        /// <param name="pickupTime">The pickup location.</param>
+
+        private int bookingId;
+        private int carId;
+        private int customerId;
+        private string pickupLocation;
+        private DateTime pickupTime;
+
+        /// Empty constructor
+        public Booking()
+        {
+
+        }
+
+        public Booking(int bookingId, int carId, int customerId, string pickupLocation, DateTime pickupTime)
+        {
+            this.BookingId = bookingId;
+            this.CarId = carId;
+            this.CustomerId = customerId;
+            this.PickupLocation = pickupLocation;
+            this.pickupTime = pickupTime;
+        }
+
+        ///Getters and setters
+        public int BookingId { get => bookingId; set => bookingId = value; }
+        public int CarId { get => carId; set => carId = value; }
+        public int CustomerId { get => customerId; set => customerId = value; }
+        public string PickupLocation { get => pickupLocation; set => pickupLocation = value; }
+        public DateTime PickupTime { get => pickupTime; set => pickupTime = value; }
+
+    }
+
+}
---
+++ Faraday Contract/Faraday Contract/Customer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faraday_Contract
+{
+    public class Customer
+    {
+
+        private string driversLicense;
+        private string name;
+
+        public Customer()
+        {
+
+        }
+
+        public Customer(string diversLicense, string name)
+        {
+            this.DriversLicense = DriversLicense;
+            this.Name = name;
+        }
+
+        public string DriversLicense { get => driversLicense; set => driversLicense = value; }
+        public string Name { get => name; set => name = value; }
+
+    }
+}
